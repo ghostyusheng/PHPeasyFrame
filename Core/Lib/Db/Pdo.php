add21b1713ed7cf27a1c227168a61c24556b79db
ZhangYuSheng <zzyss@outlook.com>
2017-07-12 18:44:31 +0800

    CURD Interface

--- Core/Lib/Db/Pdo.php
+++ Core/Lib/Db/Pdo.php
@@ -1,7 +1,6 @@
 <?php
-namespace \Core\Pdo\Db;
+namespace Core\Pdo\Db;
 
 class Pdo {
-	public function index () {
-	}
+	
 }
---
+++ Core/Lib/Db/SelectInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+interface SelectInterface {
+	
+	public function execute();		
+
+	public function select();
+
+	public function build();
+}
