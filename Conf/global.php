c6497f9b91bf540b6963b0286ef0c913542f274f
张昱升 <zhangyusheng@zhangyushengdeMacBook-Pro.local>
2018-01-07 16:01:54 +0800

    Tpl

--- Conf/global.php
+++ Conf/global.php
@@ -4,7 +4,7 @@ define('BASE_DIR', __DIR__ . '/../');
 define('CORE_DIR', __DIR__ . '/../Core/');
 define('MODEL_DIR', __DIR__ . '/../Model/');
 define('VIEW_DIR', __DIR__ . '/../Core/View/');
-define('TPL_DIR', __DIR__ . '/../Core/Tpl/');
+define('TPL_DIR', __DIR__ . '/../Tpl/');
 
 define('DB_NAME', 'buoyage');
 define('DB_HOST', '127.0.0.1');
