da36aff7e3b6da2588c1e89cfcc20927408c61ce
张昱升 <zhangyusheng@zhangyushengdeMacBook-Pro.local>
2018-01-07 18:49:00 +0800

    demo_1

--- Action/MainController.php
+++ Action/MainController.php
@@ -5,18 +5,6 @@ require "BaseController.php";
 class MainController extends BaseController
 {
 	public function index () {
-        $infos = select(
-            [
-				'*',
-				'group_concat(lon) as lons',
-				'group_concat(lat) as lats'
-            ]
-        )
-        ->from('buoyage_info')
-		->groupBy('mark_id')
-		->execute ();
-
-		$this->attach ('infos', $infos);
 		$this->show ('Index\index');
 	}
 
